9257accab916a14394d96e314560926084e036ea
Pratik-22 <pratikgawari0000@gmail.com>
2023-04-07 17:48:21 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useReducer } from 'react'
 import './App.css'
 import DigitButton from './DigitButton'
+import OperationButton from './OperationButton'
 
 export const ACTIONS = {
   ADD_DIGIT: 'ADD_DIGIT',
@@ -17,11 +18,111 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOper: payload.digit,
+          overwrite: false
+        }
+      }
+      if (payload.digit === '0' && state.currentOper === '0') {
+        return state
+      }
+      if (payload.digit === '.' && state.currentOper.includes('.')) {
+        return state
+      }
       return {
         ...state,
-        currentOper: `${currentOper || ''}${payload.digit}`
+        currentOper: `${state.currentOper || ''}${payload.digit}`
+      }
+
+    case ACTIONS.CLEAR:
+      return {}
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOper: null
+        }
+      }
+      if (state.currentOper == null) {
+        return state
+      }
+      if (state.currentOper.length === 1) {
+        return {
+          ...state,
+          currentOper: null
+        }
+      }
+      return {
+        ...state,
+        currentOper: state.currentOper.slice(0, -1)
+      }
+    case ACTIONS.EVALUATE:
+      if (state.previousOper == null || state.currentOper == null || state.operation == null) {
+        return state
+      }
+      return {
+        ...state,
+        overwrite: true,
+        previousOper: null,
+        operation: null,
+        currentOper: evaluate(state)
+      }
+
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOper == null && state.previousOper == null) {
+        return state
+      }
+      if (state.currentOper == null) {
+        return {
+          ...state,
+          operation: payload.operation
+        }
+      }
+      if (state.previousOper == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOper: state.currentOper,
+          currentOper: null
+        }
+      }
+      return {
+        ...state,
+        overwrite: true,
+        operation: payload.operation,
+        currentOper: null
       }
   }
+}
+
+function evaluate({ currentOper, previousOper, operation }) {
+  const prev = parseFloat(previousOper)
+  const current = parseFloat(currentOper)
+  if (isNaN(prev) || isNaN(current)) {
+    return ''
+  }
+  let computation = ''
+  switch (operation) {
+    case '+':
+      computation = prev + current
+      break;
+    case '-':
+      computation = prev - current
+      break;
+    case '×':
+      computation = prev * current
+      break;
+    case '÷':
+      computation = prev / current
+      break;
+    case '%':
+      computation = prev % current
+      break;
+  }
+  return computation.toString()
 }
 
 function App() {
@@ -39,26 +140,26 @@ function App() {
           {currentOper}
         </div>
       </div>
-      <button>AC</button>
-      <button>( )</button>
-      <button>%</button>
-      <DigitButton digit='÷' dispatch={dispatch}></DigitButton>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>×</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>-</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>+</button>
-      <button>0</button>
-      <button>.</button>
-      <button>DEL</button>
-      <button>=</button>
+      <button onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
+      <DigitButton digit='' dispatch={dispatch}></DigitButton>
+      <OperationButton operation='%' dispatch={dispatch}></OperationButton>
+      <OperationButton operation='÷' dispatch={dispatch}></OperationButton>
+      <DigitButton digit='7' dispatch={dispatch}></DigitButton>
+      <DigitButton digit='8' dispatch={dispatch}></DigitButton>
+      <DigitButton digit='9' dispatch={dispatch}></DigitButton>
+      <OperationButton operation='×' dispatch={dispatch}></OperationButton>
+      <DigitButton digit='4' dispatch={dispatch}></DigitButton>
+      <DigitButton digit='5' dispatch={dispatch}></DigitButton>
+      <DigitButton digit='6' dispatch={dispatch}></DigitButton>
+      <OperationButton operation='-' dispatch={dispatch}></OperationButton>
+      <DigitButton digit='1' dispatch={dispatch}></DigitButton>
+      <DigitButton digit='2' dispatch={dispatch}></DigitButton>
+      <DigitButton digit='3' dispatch={dispatch}></DigitButton>
+      <OperationButton operation='+' dispatch={dispatch}></OperationButton>
+      <DigitButton digit='0' dispatch={dispatch}></DigitButton>
+      <DigitButton digit='.' dispatch={dispatch}></DigitButton>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
+      <button onClick={() => dispatch({ type: ACTIONS.EVALUATE })}>=</button>
     </div>
   )
 }
--- src/DigitButton.jsx
+++ src/DigitButton.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { ACTIONS } from './App'
+import { ACTIONS } from "./App"
 
-const DigitButton = ({ dispatch, digit }) => {
+export default function DigitButton({ dispatch, digit }) {
+  // dispatch prop: is a function that takes an action object as an argument and is used to update the state of the parent component (App)
+  // digit prop: is a string representing the digit displayed on the button.
+  
   return (
-    <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}>
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
       {digit}
     </button>
   )
 }
-
-export default DigitButton
